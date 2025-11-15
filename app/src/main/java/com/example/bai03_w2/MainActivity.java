2761144543058a2dc0514187673b050af4902cdb
nguyenkhang540 <nguyenvikhang540@gmail.com>
2025-11-15 15:53:28 +0700

    First commit

---
+++ app/src/main/java/com/example/bai03_w2/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.bai03_w2;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_singup);
+
+
+
+    }
+}
